489a666f74f881ca998d9f637d398892533390d3
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-10-20 21:54:50 +0100

    adding to parser

--- parser.c
+++ parser.c
@@ -10,42 +10,43 @@
  * Return: characters total
  */
 
-int parser(const char *format, checker f_lists[], va_list arg_list)
+int parser(const char *format, checker f_list[], va_list arg_list)
 {
 	int i, j, r_val, printed_chars;
 
 	printed_chars = 0;
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') /*Checks for format specifiers*/
 		{
+			/*Iterates through struct to find the right func*/
 			for (j = 0; f_list[j].sym != NULL; j++)
 			{
-				if (format[i + 1] == f_lists[j].sym[0])
+				if (format[i + 1] == f_list[j].sym[0])
 				{
-					r_val = f_lists[j].f(arg_list);
+					r_val = f_list[j].f(arg_list);
 					if (r_val == -1)
 						return (-1);
 					printed_chars += r_val;
 					break;
 				}
 			}
-			if (f_lists[j].sym == NULL && format[i + 1] != ' ')
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
+					_write_char(format[i]);
+					_write_char(format[i + 1]);
 					printed_chars = printed_chars + 2;
 				}
 				else
 					return (-1);
 			}
-			i = i + 1;
+			i = i + 1; /*Updating i to skip format symbols*/
 		}
 		else
 		{
-			_putchar(format[i]);
+			_write_char(format[i]); /*call the write function*/
 			printed_chars++;
 		}
 	}
