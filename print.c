e0e9c9dbfd1e9b62f29427e4b19f22088c2aa31a
Ann <111051400+Horupey@users.noreply.github.com>
2022-10-21 22:53:47 +0100

    Update print.c

--- print.c
+++ print.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
+ * handle_print - Prints an parameters based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of parameters to be printed.
  * @ind: ind.
