9b4b08069acc1d036b2feb4e835205e10cee8cf9
Ann <111051400+Horupey@users.noreply.github.com>
2022-10-21 22:56:24 +0100

    Update width.c

--- width.c
+++ width.c
@@ -4,7 +4,7 @@
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
- * @list: list of parameters.
+ * @list: list of arguments.
  *
  * Return: width.
  */
